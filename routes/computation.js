c7caaa4e258cbf4b7db96654a0fa51d7d77c06bf
Ambica Peruru <ambicaperuru@gmail.com>
2022-10-26 21:47:06 -0500

    Worked on computing functions

--- routes/computation.js
+++ routes/computation.js
@@ -3,18 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var num1=Math.abs(Math.random() * 10)
-  var num2=Math.abs(Math.random() * 10)
-  var num3=Math.abs(Math.random() * 10)
 
   var fround = Math.fround(num1)
   var random = Math.random(num2)
   var round = Math.round(num3)
 
   res.render('computation', { title: 'Ambica Peruru computing functions' ,
-  num1 : num1,
-  num2 : num2,
-  num3 : num3,
   fround : fround,
   random : random,
   round : round
